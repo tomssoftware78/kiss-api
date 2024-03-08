49512218e41d443cdd644e7d574a7a9136106ad3
Rob Van Dyck <rob.vandyck@police.belgium.eu>
2024-03-08 15:16:01 +0100

    Support the old iris version (production)

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,34 @@
 #
 FROM python:3.9
+RUN apt-get update
+RUN apt-get install -y apt-utils build-essential gcc
 
+ENV JAVA_FOLDER java-se-8u41-ri
+
+ENV JVM_ROOT /usr/lib/jvm
+
+ENV JAVA_PKG_NAME openjdk-8u41-b04-linux-x64-14_jan_2020.tar.gz
+ENV JAVA_TAR_GZ_URL https://download.java.net/openjdk/jdk8u41/ri/$JAVA_PKG_NAME
+
+RUN apt-get update && apt-get install -y wget && rm -rf /var/lib/apt/lists/*    && \
+    apt-get clean                                                               && \
+    apt-get autoremove                                                          && \
+    echo Downloading $JAVA_TAR_GZ_URL                                           && \
+    wget -q $JAVA_TAR_GZ_URL                                                    && \
+    tar -xvf $JAVA_PKG_NAME                                                     && \
+    rm $JAVA_PKG_NAME                                                           && \
+    mkdir -p /usr/lib/jvm                                                       && \
+    mv ./$JAVA_FOLDER $JVM_ROOT                                                 && \
+    update-alternatives --install /usr/bin/java java $JVM_ROOT/$JAVA_FOLDER/bin/java 1        && \
+    update-alternatives --install /usr/bin/javac javac $JVM_ROOT/$JAVA_FOLDER/bin/javac 1     && \
+    java -version
 #
 WORKDIR /code
 
 #
 COPY ./requirements.txt /code/requirements.txt
 COPY ./driver/intersystems_irispython-3.2.0-py3-none-any.whl /code/intersystems_irispython-3.2.0-py3-none-any.whl
+COPY ./driver/CacheDB.jar /code/app/driver/CacheDB.jar
 
 #
 RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
--- app/kissutils.py
+++ app/kissutils.py
@@ -8,13 +8,102 @@ from typing import Optional
 from socket import socket
 from fabric import Connection
 
+import jaydebeapi
+from dotenv import load_dotenv
+load_dotenv()
+use_cache_driver = os.environ.get('USE_OLD_CACHE_DRIVER')
+
 Endpoint = str
+
+class CacheConnectionManager(BaseConnectionManager[Endpoint, jaydebeapi.Connection]):
+    """We don't want to create a new connection each time, because the db connection fails the first time right after
+    creating the ssh-tunnel. It all takes too much time. """
+    def __init__(self, connection_kiss_ip, connection_kiss_port: str, connection_kiss_schema: str, username: str,
+                 password: str, connection_ssh_jump_key: str, connection_ssh_jump_user: str, connection_ssh_jump_host: str):
+        print("Instantiating CacheConnectionManager")
+        self.connection_kiss_ip = connection_kiss_ip
+        self.connection_kiss_port = connection_kiss_port
+        self.connection_kiss_schema = connection_kiss_schema
+        self.connection_kiss_username = username
+        self.connection_kiss_password = password
+        self.connection_ssh_jump_key = connection_ssh_jump_key
+        self.connection_ssh_jump_user = connection_ssh_jump_user
+        self.connection_ssh_jump_host = connection_ssh_jump_host
+        self.connectiondata = {}
+
+    # First connection takes some time
+    def retrier(self, counter, func, args):
+        try:
+            return func(*args)
+        except Exception as e:
+            print("Retrier: " + str(counter) + " " + str(e))
+            if counter > 0:
+                time.sleep(1)
+                return self.retrier(counter - 1, func, args)
+            else:
+                raise e
+
+    def connect_to_iris(self, localport):
+        connection_string = "127.0.0.1:" + str(localport) + "/" + self.connection_kiss_schema
+
+        JAR_FILE = "driver/CacheDB.jar"
+
+        print("Driver file found: " + str(os.path.exists(JAR_FILE)))
+        IRIS_DRIVER = "com.intersys.jdbc.CacheDriver"
+
+        return jaydebeapi.connect(IRIS_DRIVER, "jdbc:Cache://"+connection_string, {"user":"root_kiss", "password":"kiss001"}, [JAR_FILE])
+
+    def create(self, endpoint: Endpoint, timeout: Optional[float] = None) -> jaydebeapi.Connection:
+        localport = None
+        with socket() as s:
+            s.bind(('', 0))
+            print(s.getsockname()[1])
+            localport = (int)(s.getsockname()[1])
+
+        networkconnection = Connection(
+            host=self.connection_ssh_jump_host,
+            user=self.connection_ssh_jump_user,
+            connect_kwargs={
+                "key_filename": self.connection_ssh_jump_key,
+            },
+        )
+        ctx = networkconnection.forward_local(local_port=localport, remote_port=self.connection_kiss_port,
+                       remote_host=self.connection_kiss_ip, local_host="127.0.0.1")
+        ctx.__enter__()
+
+        db_connection = self.retrier(3, self.connect_to_iris, [(localport)])
+
+        self.connectiondata[db_connection] = (networkconnection, ctx, localport)
+
+        print("Created a new connection with ssh port: " + str(self.connectiondata[db_connection][2]))
+
+        return db_connection
+
+    def dispose(self, endpoint: Endpoint, db_connection: jaydebeapi.Connection, timeout: Optional[float] = None) -> None:
+        self.connectiondata.pop(db_connection)
+
+    def check_aliveness(self, endpoint: Endpoint, db_connection: jaydebeapi.Connection, timeout: Optional[float] = None) -> bool:
+        try:
+            cursor = db_connection.cursor()
+            cursor.execute("SELECT TOP 1 ID FROM kiss.tblGebruikers")
+        except Exception as e:
+            return False
+
+        return True
+
+    def on_acquire(self, endpoint: Endpoint, db_connection: jaydebeapi.Connection) -> None:
+        print("Acquired a new connection with ssh port: " + str(self.connectiondata[db_connection][2]))
+
+
+    def on_release(self, endpoint: Endpoint, db_connection: jaydebeapi.Connection) -> None:
+        print("Released a connection with ssh port: " + str(self.connectiondata[db_connection][2]))
 
 class IrisConnectionManager(BaseConnectionManager[Endpoint, iris.IRISConnection]):
     """We don't want to create a new connection each time, because the db connection fails the first time right after
     creating the ssh-tunnel. It all takes too much time. """
     def __init__(self, connection_kiss_ip, connection_kiss_port: str, connection_kiss_schema: str, username: str,
                  password: str, connection_ssh_jump_key: str, connection_ssh_jump_user: str, connection_ssh_jump_host: str):
+        print("Instantiating IrisConnectionManager")
         self.connection_kiss_ip = connection_kiss_ip
         self.connection_kiss_port = connection_kiss_port
         self.connection_kiss_schema = connection_kiss_schema
@@ -103,10 +192,19 @@ class Database:
                 connection_ssh_jump_user = os.environ.get('CONNECTION_SSH_JUMP_USER')
                 connection_ssh_jump_host = os.environ.get('CONNECTION_SSH_JUMP_HOST')
 
-                self._connection_pool = ConnectionPool[Endpoint, Connection](
-                    IrisConnectionManager(connection_kiss_ip, connection_kiss_port, connection_kiss_schema,
+                connection_manager = None
+                print("use_cache_driver: " + use_cache_driver)
+                if use_cache_driver == 'True':
+                    connection_manager = CacheConnectionManager(connection_kiss_ip, connection_kiss_port, connection_kiss_schema,
                                           connection_kiss_username, connection_kiss_password, connection_ssh_jump_key,
-                                          connection_ssh_jump_user, connection_ssh_jump_host),
+                                          connection_ssh_jump_user, connection_ssh_jump_host)
+                else:
+                    connection_manager = IrisConnectionManager(connection_kiss_ip, connection_kiss_port, connection_kiss_schema,
+                                          connection_kiss_username, connection_kiss_password, connection_ssh_jump_key,
+                                          connection_ssh_jump_user, connection_ssh_jump_host)
+
+                self._connection_pool = ConnectionPool[Endpoint, Connection](
+                    connection_manager,
                     idle_timeout=30.0,
                     max_lifetime=6000.0,
                     min_idle=1,
