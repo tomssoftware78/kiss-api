4a5e0675aa06f29d957b418e07d8238abbe1ffdc
Tom Van den Eynde <tom.vandeneynde@police.belgium.eu>
2025-12-17 16:39:44 +0100

    enables deployment multiple times after each other

--- scripts_deploy/startup_at_docker_host03.sh
+++ scripts_deploy/startup_at_docker_host03.sh
@@ -5,9 +5,9 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PROJECT_ROOT="$( cd "$SCRIPT_DIR/.." && pwd )"
 
 rm $PROJECT_ROOT/docker-compose.yml
-mv $PROJECT_ROOT/docker-compose-docker-host03.yml $PROJECT_ROOT/docker-compose.yml
+cp $PROJECT_ROOT/docker-compose-docker-host03.yml $PROJECT_ROOT/docker-compose.yml
 rm -f $PROJECT_ROOT/app/logging_config.yml
-mv $PROJECT_ROOT/app/logging_config_tmp_prd.yml $PROJECT_ROOT/app/logging_config.yml
+cp $PROJECT_ROOT/app/logging_config_tmp_prd.yml $PROJECT_ROOT/app/logging_config.yml
 
 docker build -t i2-kiss-api . 
 docker compose up -d
